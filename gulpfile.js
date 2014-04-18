99aed7ec10da0969632ab295cd5018603c94d1f5
Florian Reiterer <me@florianreiterer.com>
2014-04-18 16:32:18 +0200

    update for new gulp-sourcemaps version

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
+'use strict';
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var sourcemap = require('gulp-sourcemaps');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('javascript', function() {
-	gulp.src('src/**/*.js')
-	.pipe(concat('all.js'))
-	.pipe(uglify({outSourceMap: true}))
-	.pipe(sourcemap())
-	.pipe(gulp.dest('dist'));
+  gulp.src('src/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', ['javascript']);
